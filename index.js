4dbf685dc275e774fbe843e0552e4ee522e2a2fa
Abdulazeez Saidu <saeeduabdulazeez@gmail.com>
2021-07-17 01:58:35 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Dark Mode
-
+/*
 const sun = document.querySelector(".sun")
 const body = document.querySelector("body")
 
 sun.onclick = function() {
     body.classList.toggle("dark");
 
-}
+}*/
